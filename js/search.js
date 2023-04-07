3ca2e8b8fc63bc0cf83e56f93fc46ed5804f0db6
LamiaChebel <lamia.chebel@3wa.io>
2023-04-07 17:12:49 +0200

    update_4_API_Movies

--- js/search.js
+++ js/search.js
@@ -1,16 +1,23 @@
 let query="";
+const article = document.createElement("article");
+
 let input = document.querySelector("input");
 
 function getInput(){
     return input.value;
 }
 
+// function onClickDetails(){
+
+//     document.querySelectorAll("aside").classList.toggle('hide');
+
+// }
 
 function fetchResults(){
 
-    let url = new URL(`https://api.themoviedb.org/3/search/movie?api_key=58051e21a60016eadf0e959e12d53e24&language=en-US&query=${getInput()}`);
+    let urlSearch = new URL(`https://api.themoviedb.org/3/search/movie?api_key=58051e21a60016eadf0e959e12d53e24&language=en-US&query=${getInput()}`);
     
-    fetch(url)
+    fetch(urlSearch)
         .then(response => response.json())
         .then(response => {
                 const searchPage = document.querySelector("#searchPage")
@@ -24,7 +31,6 @@ function fetchResults(){
 
                 for (const result of response.results) {
 
-                    const article = document.createElement("article");
                     document.querySelector("#searchResults").appendChild(article);
 
 
@@ -38,40 +44,55 @@ function fetchResults(){
                     image.setAttribute("src", `https://image.tmdb.org/t/p/w300/${result.poster_path}`);
                     image.setAttribute("alt", "");
 
-
+                    const aside = document.createElement(aside);
+                    // aside.setAttribute("class","hide");
+                    article.appendChild(aside);
+                    console.log(aside);
+                    
+                
                     const p1 = document.createElement("p");
-                    article.appendChild(p1);
+                    aside.appendChild(p1);
                     p1.textContent = `vote count : ${result.vote_count}`;
-
-
+                
+                
                     const p2 = document.createElement("p");
-                    article.appendChild(p2);
+                    aside.appendChild(p2);
                     p2.textContent = `vote average : ${result.vote_average}`;
-
-
+                
+                
                     const p3 = document.createElement("p");
-                    article.appendChild(p3);
+                    aside.appendChild(p3);
                     p3.textContent = `release : ${new Date(result.release_date).toLocaleDateString()}`;
-
+                
                     const p4 = document.createElement("p");
-                    article.appendChild(p4);
+                    aside.appendChild(p4);
                     p4.textContent = `${result.overview}`;
+                
+                    let urlDetails = new URL(`https://api.themoviedb.org/3/movie/${result.id}?api_key={58051e21a60016eadf0e959e12d53e24}&append_to_response=production_companies`)
+                    
+                    fetch(urlDetails)
+                        .then(res=> res.json())
+                            .then(res=> {
+                               for(const prodName of res.production_companies) {
+                                const company = document.createElement("a");
+                                aside.appendChild(company);
+                                const companyInfos={
+                                    id:prodName.id,
+                                    name:prodName.name,
+                                };
 
-                    const company = document.createElement("a");
-                    console.log(company);
-                    article.appendChild(a).setAttribute("href","");
-                    company.textContent =""; 
-
-
-
-
-                }
+                                console.log(companyInfos);
+                               }
+                               
+                            });
+               
+                             
             
-        })
+                };
+            })
         .catch(err => console.error(err));
-}
-
-function ResetResults(e){
+        };        
+function ResetResults(){
     if(document.querySelector("#searchResults")){
         document.querySelector("#searchResults").remove();
     }
@@ -89,4 +110,8 @@ document.addEventListener("DOMContentLoaded", function () {
     
     document.querySelector("form").addEventListener("submit", submitRequest );
     input.addEventListener("click",ResetResults);
-})
+    // document.querySelectorAll("img").addEventListener("click",onClickDetails);
+
+
+
+});
